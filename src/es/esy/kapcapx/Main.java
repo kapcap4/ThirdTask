ce63ba31684dcffe618d8906232c9be7fe9db252
Kapcap4 <kapcap43@gmail.com>
2017-10-23 23:05:13 +0400

    1 sub-task

--- src/es/esy/kapcapx/Main.java
+++ src/es/esy/kapcapx/Main.java
@@ -3,6 +3,14 @@ package es.esy.kapcapx;
 public class Main {
 
     public static void main(String[] args) {
-
+        comparison(-10, -11);
+    }
+    // Метод сравнения
+    public static void comparison (int a, int b) {
+        if ((a + b) > (a - b)) {
+            System.out.println("true" + " (a + b) = " + "[" + (a + b) + "]");
+        } else {
+            System.out.println("false" + " (a - b) = " + "[" + (a - b) + "]");
+        }
     }
 }
